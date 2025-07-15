672ca722efad9f346ce7df35bb6cf5734c34750d
Jean-mnlv <jj.enama@mnlvm.com>
2025-07-15 02:35:05 +0100

    Update and rename api.js to api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
-// Préconfiguration Axios (exemple)
-import axios from "axios";
-const api = axios.create({
+import axios, { AxiosInstance } from "axios";
+
+const api: AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "http://localhost:3001/api",
 });
+
 export default api;
